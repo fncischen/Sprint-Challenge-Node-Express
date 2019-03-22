6d5a27595bc87be2b6a5860bc11492680f2446e6
Francis Chen <fncis.chen@gmail.com>
2019-03-22 09:55:29 -0700

    refactored action and project CRUD methods, added actions router

--- routers/actions.js
+++ routers/actions.js
@@ -2,14 +2,68 @@ const express = require('express');
 const router = express.Router();
 const actionData = require("../data/helpers/actionModel.js");
 
-router.get("/" , async (req, res) => { })
+router.get("/" , async (req, res) => {
+    try{
+        const actions = await actionData.get();
+        res.status(201).json(actions);
+    }
+    catch
+    {
+        res.status(500).json({errorMessage: "We had errors retrieving your actions"});
+    }
 
-router.get("/:id", async (req, res) => { })
+ })
 
-router.post("/", async (req, res) => { })
+router.get("/:id", async (req, res) => { 
 
-router.put("/:id", async (req, res) => { })
+    try{
+        const action = await actionData.get(req.params.id);
+        res.status(201).json(action);
+    }
+    catch
+    {
+        res.status(500).json({errorMessage: "We had errors retrieving your action"});
+    }
 
-router.delete("/:id", async (req, res) => { })
+})
+
+router.post("/", async (req, res) => { 
+
+    try{
+        const action = await actionData.get(req.params.id);
+        res.status(201).json(action);
+    }
+    catch
+    {
+        res.status(500).json({errorMessage: "We had errors posting your action"});
+    }
+
+})
+
+router.put("/:id", async (req, res) => { 
+
+    try{
+        const action = await actionData.get(req.params.id);
+        res.status(201).json(action);
+    }
+    catch
+    {
+        res.status(500).json({errorMessage: "We had errors updating your action"});
+    }
+
+})
+
+router.delete("/:id", async (req, res) => { 
+
+    try{
+        const action = await actionData.get(req.params.id);
+        res.status(201).json(action);
+    }
+    catch
+    {
+        res.status(500).json({errorMessage: "We had errors deleting your action"});
+    }
+
+})
 
 module.export = router;
--- routers/projects.js
+++ routers/projects.js
@@ -4,58 +4,58 @@ const projectData = require("../data/helpers/projectModel.js");
 
 router.get("/" , async (req, res) => { 
     try{
-        const posts = await projectData.get();
-        res.status(201).json(posts);
+        const project = await projectData.get();
+        res.status(201).json(project);
     }
     catch
     {
-        res.status(500).json({errorMessage: "We had errors retrieving your posts"});
+        res.status(500).json({errorMessage: "We had errors retrieving your projects"});
     }
 })
 
 router.get("/:id", async (req, res) => { 
     try{
-        const posts = await projectData.get(req.params.id);
-        res.status(201).json(posts);
+        const project = await projectData.get(req.params.id);
+        res.status(201).json(project);
     }
     catch
     {
-        res.status(500).json({errorMessage: "We had errors retrieving your posts"});
+        res.status(500).json({errorMessage: "We had errors retrieving your project"});
     }
 })
 
 router.post("/", async (req, res) => { 
     try{
-        const posts = await projectData.insert(req.body);
-        res.status(201).json(posts);
+        const project = await projectData.insert(req.body);
+        res.status(201).json(project);
     }
     catch
     {
-        res.status(500).json({errorMessage: "We had errors retrieving your posts"});
+        res.status(500).json({errorMessage: "We had errors posting your project"});
     }
 })
 
 router.put("/:id", async (req, res) => { 
 
     try{
-        const posts = await projectData.update(req.params.id, req.body);
-        res.status(201).json(posts);
+        const project = await projectData.update(req.params.id, req.body);
+        res.status(201).json(project);
     }
     catch
     {
-        res.status(500).json({errorMessage: "We had errors retrieving your posts"});
+        res.status(500).json({errorMessage: "We had errors updating your project"});
     }
 })
 
 router.delete("/:id", async (req, res) => { 
 
     try{
-        const posts = await projectData.remove(req.params.id);
-        res.status(201).json(posts);
+        const project = await projectData.remove(req.params.id);
+        res.status(201).json(project);
     }
     catch
     {
-        res.status(500).json({errorMessage: "We had errors retrieving your posts"});
+        res.status(500).json({errorMessage: "We had errors deleting your project"});
     }
 
 })
