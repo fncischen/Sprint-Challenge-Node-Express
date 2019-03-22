3312ede4adf6822f62898b5801bd7d696bc42e25
Francis Chen <fncis.chen@gmail.com>
2019-03-22 09:27:48 -0700

    added routers with necessary imports

---
+++ routers/actions.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const router = express.Router();
+const actionData = require("../data/helpers/actionModel.js");
---
+++ routers/posts.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const router = express.Router();
+const postData = require("../data/helpers/projectModel.js");
