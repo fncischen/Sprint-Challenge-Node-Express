fea9a0a27f6395e5029e7272996f533feaaa8781
Francis Chen <fncis.chen@gmail.com>
2019-03-22 12:01:18 -0700

    fixed actions middleware

--- data/middleware/actionsMiddleware.js
+++ data/middleware/actionsMiddleware.js
@@ -1,6 +1,6 @@
 const actionsMiddleware = fn => (req, res, next) => {
     console.log(req.body);
-    if (!req.body.project_id || !req.body.name || !req.body.description) {
+    if (!req.body.project_id || !req.body.description || !req.body.notes) {
         res.status(500).json({error: "One of your fields is empty and needs to be filled out"})
       } else {
         console.log("Passing action data!");
