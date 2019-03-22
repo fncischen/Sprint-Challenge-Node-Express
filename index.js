c5f542065d44dac12ae4be848eb66629f505d70d
Francis Chen <fncis.chen@gmail.com>
2019-03-22 09:23:20 -0700

    set up structure for Server Routing

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
+const express = require('express');
+const server = express();
+
+const actionRoutes = require("./routers/actions.js");
+const postRoutes = require("./routers/posts.js");
+
+server.use(express.json());
+server.use('/actions', actionRoutes);
+server.use('/posts', postRoutes);
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () =>
+console.log('Server running on http://localhost:5000')
+);
