292498a9d97a9da1b95e0383df74f6e70ccd7fa3
Francis Chen <fncis.chen@gmail.com>
2019-03-22 09:30:43 -0700

    set up structure for individual routers

--- routers/actions.js
+++ routers/actions.js
@@ -1,3 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const actionData = require("../data/helpers/actionModel.js");
+
+router.get((req, res) => { })
+
+router.get((req, res) => { })
+
+router.post((req, res) => { })
+
+router.put((req, res) => { })
+
+router.delete((req, res) => { })
+
+module.export = router;
--- routers/posts.js
+++ routers/posts.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const postData = require("../data/helpers/projectModel.js");
+
+router.get(() => { })
+
+router.get(() => { })
+
+router.post(() => { })
+
+router.put(() => { })
+
+router.delete(() => { })
